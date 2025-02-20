c47c911eb77cb86894ada69ae1198c0a54c29aab
Ivan_Karagachev <ivan.karagachev@gmail.com>
2025-02-20 12:36:15 +0300

    tasts 1 & 2

---
+++ ikbo-36-22-karagachev-app2/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainContent.css';
+
+function MainContent() {
+    return (
+        <main className='main-content'>
+            <h1>Второе приложение</h1>
+            <p>Главная страница</p>
+        </main>
+    );
+}
+
+export default MainContent; 
